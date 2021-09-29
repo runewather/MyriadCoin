4991ca9d230236a09585e133a578936d31123cda
Victo Principe <victo_10_@hotmail.com>
2021-09-29 12:08:35 -0300

    Added a new test case to check if the tokens can be transferred

--- test/Myriad.test.js
+++ test/Myriad.test.js
@@ -1,11 +1,31 @@
 const Myriad = artifacts.require("Myriad");
 
 contract("Myriad", (accounts) => {
+  let myriad = null;
   before(async () => {
     myriad = await Myriad.deployed();
   });
 
-  it("Gives the owner of the token 1M tokens", async () => {
+  it("Gives the owner of the token 100M tokens", async () => {
     let balance = await myriad.balanceOf(accounts[0]);
+    balance = web3.utils.fromWei(balance, "ether");
+    assert.equal(
+      balance,
+      "100000000",
+      "Balance should be 100M for contract creator"
+    );
+  });
+
+  it("Can transfer tokens between accounts", async () => {
+    let amount = web3.utils.toWei("100", "ether");
+    await myriad.transfer(accounts[1], amount, { from: accounts[0] });
+
+    let balance = await myriad.balanceOf(accounts[1]);
+    balance = web3.utils.fromWei(balance, "ether");
+    assert.equal(
+      balance,
+      "100",
+      "Balance should be 100 for the account recipient"
+    );
   });
 });
